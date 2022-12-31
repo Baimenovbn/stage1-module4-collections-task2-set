2bb1e38d4eb0298068353eadcb5d8e5187187e10
Baimenovbn <baibn@mail.ru>
2022-12-31 14:36:17 +0600

    feat: finish task with better performance

--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -2,25 +2,19 @@ package com.epam.mjc.collections.set;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.function.Consumer;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
-        HashSet<String> result = new HashSet<>();
+        HashSet<String> firstSecondIntersectionAndNotInThird = new HashSet<>(firstSet);
+        firstSecondIntersectionAndNotInThird.retainAll(secondSet);
+        firstSecondIntersectionAndNotInThird.removeAll(thirdSet);
 
-        Consumer<String> consumer = el -> {
-            if (firstSet.contains(el) && secondSet.contains(el) && !thirdSet.contains(el)) {
-                result.add(el);
-            } else if (!firstSet.contains(el) && !secondSet.contains(el) && thirdSet.contains(el)) {
-                result.add(el);
-            }
-        };
+        HashSet<String> onlyToThird = new HashSet<>(thirdSet);
+        onlyToThird.removeAll(firstSet);
+        onlyToThird.removeAll(secondSet);
 
-        firstSet.forEach(consumer);
-        secondSet.forEach(consumer);
-        thirdSet.forEach(consumer);
+        firstSecondIntersectionAndNotInThird.addAll(onlyToThird);
 
-
-        return result;
+        return firstSecondIntersectionAndNotInThird;
     }
 }
