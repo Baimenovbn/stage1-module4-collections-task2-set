2f98d05729bb59952eebaeda764b0878c7ae7b98
Baimenovbn <baibn@mail.ru>
2022-12-31 14:24:28 +0600

    feat: finish task

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,23 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> result = new HashSet<>();
+
+        sourceList.forEach(el -> {
+            int copy = el;
+
+            if (copy % 2 == 0) {
+                do {
+                    result.add(copy);
+                    copy /= 2;
+                } while(copy % 2 == 0);
+                result.add(copy);
+            } else {
+                result.add(el);
+                result.add(el * 2);
+            }
+        });
+
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -2,8 +2,25 @@ package com.epam.mjc.collections.set;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Consumer;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+        HashSet<String> result = new HashSet<>();
+
+        Consumer<String> consumer = el -> {
+            if (firstSet.contains(el) && secondSet.contains(el) && !thirdSet.contains(el)) {
+                result.add(el);
+            } else if (!firstSet.contains(el) && !secondSet.contains(el) && thirdSet.contains(el)) {
+                result.add(el);
+            }
+        };
+
+        firstSet.forEach(consumer);
+        secondSet.forEach(consumer);
+        thirdSet.forEach(consumer);
+
+
+        return result;
     }
 }
